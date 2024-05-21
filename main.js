53b1f1f824614f23b7bec5d0e28ba4f5837f8ae3
Aisha-Farah <131600327+Aisha-Farah@users.noreply.github.com>
2024-05-21 09:08:33 +0430

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+// let firstName = "Aisha";
+// let lastName = "Farah";
+// let fullName = firstName + " " + lastName ;
+// document.write("fullname is: "+fullName);
+
+
+// let x= 5;
+// let y = 7;
+// let z = x+y;
+
+// document.write("sum of x and y is: "+z);
+
+
+let age = 1997;
+
+const year = 2024;
+document.write(year - age); 
